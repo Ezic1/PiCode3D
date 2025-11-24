90d19a36df06e9a28929cc4dc4e0f7f8056a5d5c
Ezic1 <mateocasas000@gmail.com>
2025-11-24 00:21:51 -0300

    Se definieron los GPIOS

---
+++ hardware/gpio_pins.py
@@ -0,0 +1,29 @@
+# gpio_pins.py
+"""
+Archivo central donde se definen todos los pines GPIO usados por el hardware.
+Mantiene el código limpio y facilita modificaciones.
+"""
+
+# ==========================
+# Pines de salida
+# ==========================
+RELAY_PIN = 17        # Encender energía impresora
+RELAY_OFF_PIN = 27    # Apagar energía impresora
+BUZZER_PIN = 22       # Buzzer activo
+FAN_PIN = 12          # Ventilador para Pi PWM PIN
+STATUS_LED_PIN = 23   # LED indicador opcional PWM Simulado
+
+# ==========================
+# Pines de entrada
+# ==========================
+BUTTON_PIN = 21       # Botón físico de power/control
+RELAY_STATUS_PIN = 26 # Estado de la relé   
+
+# ==========================
+# Pines especiales
+# ==========================
+I2C_SDA_PIN = 2
+I2C_SCL_PIN = 3
+
+UART_TX_PIN = 14
+UART_RX_PIN = 15
